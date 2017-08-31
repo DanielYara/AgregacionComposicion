b68efa9f475dcaf961c1a5a4129f83d2d85b0f48
Estudiante <Estudiante@10.203.3.21>
2017-08-30 20:59:00 -0500

    Ejercicio Clase 31 Agosto

---
+++ src/agregacióncomposición/Estudiante.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agregacióncomposición;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Estudiante {
+    private String nombre;
+    private int codigo;
+
+    public Estudiante(int codigo, String nombre) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+    
+    
+}
+    
+   
